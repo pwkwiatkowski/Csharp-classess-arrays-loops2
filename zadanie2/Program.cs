a8ae5ebbad0317b607cabed7d96ed86d012e8024
Patryk Kwiatkowski <pwikkwiatkowski@gmail.com>
2021-05-11 00:47:11 +0200

    Add project files.

---
+++ zadanie2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Utwórz postrzępioną tablicę liczb typu int.Wypełnij kolejne komórki wartościami malejącymi od 10 do 1.
+            //Do utworzenia tablicy i wypełnienia jej danymi wykorzystaj pętlę typu for.
+
+            int licznik = 10;
+
+            int[][] postrzepiona = new int[4][];
+
+            for(int i=0; i<postrzepiona.Length; i++)
+            {
+                postrzepiona[i] = new int[i + 1];
+
+                for(int j=0; j<postrzepiona[i].Length; j++)
+                {
+                    postrzepiona[i][j] = licznik--;
+                    Console.Write("{0} ", postrzepiona[i][j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
